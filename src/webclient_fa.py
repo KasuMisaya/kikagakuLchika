b73a461c0c3461c6bf525ba1acacef3d6a50460b
KauMisaya <turumaru20101534@yahoo.co.j>
2023-10-22 02:17:37 +0900

    fix

--- src/webclient_fa.py
+++ src/webclient_fa.py
@@ -17,5 +17,5 @@ async def get_command():
     command_to_run = None  # コマンドをリセット
     return {"command": command}
 
-if __name__ == "__main__":
-    uvicorn.run("jarvis_webclient_fa:app", host="192.168.2.151", port=8002, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("jarvis_webclient_fa:app", host="192.168.2.151", port=8002, reload=True)

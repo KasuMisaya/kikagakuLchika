370e1a1a0ecffbfddee445a31185cbdfa23d2d2f
KauMisaya <turumaru20101534@yahoo.co.j>
2023-10-28 19:30:25 +0900

    機能追加

--- src/webclient_st.py
+++ src/webclient_st.py
@@ -3,6 +3,7 @@ import streamlit as st
 import requests
 
 FASTAPI_SERVER = "http://127.0.0.1:8000"
+FASTAPI_SERVER = "https://everyone-l-chika.onrender.com"
 headers = {
     'Content-Type': 'application/json',
     'accept': 'application/json'
